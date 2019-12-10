03a5950c2b8fa913d38f296caf7a87c1735a3977
Stewart Sum <stewartsum@users.noreply.github.com>
2019-12-10 00:33:53 -0700

    Updates

---
+++ precache-manifest.c646feabad1861c0382355f4f578bbb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f4068a15e78364c6e7c9b72ae33fee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bdc826350f995ca60d82",
+    "url": "/monsters-rolodex/static/css/main.e7e51795.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "bdc826350f995ca60d82",
+    "url": "/monsters-rolodex/static/js/main.09ea61ee.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
